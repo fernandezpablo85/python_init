2328636daf1d97a8c38feeea56976303843a7e90
taniavill <tvillanueva@despegar.com>
2020-04-27 17:55:25 -0300

    change print for return

--- numbers/number.py
+++ numbers/number.py
@@ -1,14 +1,14 @@
 def add(a, b):
-    print(a+b)
+    return(a+b)
 
 def is_even(n):
-    print(n%2 == 0)
+    return(n%2 == 0)
     
 def is_odd(n):
-    print(n%2 != 0)
+    return(n%2 != 0)
 
 def sum_all(arr):
     add_all = 0
     for numb in arr:
         add_all = (add_all + numb)
-    print(add_all)
+    return(add_all)
