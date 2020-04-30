0b290fd244875c55aac62c7154536f5b3ad1b178
taniavill <tvillanueva@despegar.com>
2020-04-30 14:17:03 -0300

    nueva función para dibujar triángulo
    Tampoco pasa el test

--- triangles/triangle.py
+++ triangles/triangle.py
@@ -1,10 +1,19 @@
-def draw(n):
-    for i in range(0,n):
-        for j in range(0,i+1):
-            print('*',end=' ')
-        print ('\n')
+def draw(n,t=1):
+    while n > 0:
+        print('* '*(n-n+t))
+        return draw(n-1,t+1)
 
-
-def drawn(n):
-    for i in range(0,n):
-        print((i+1)*'* ')
+def draws(i):
+    #if i == 0:
+    #    return ""
+    #else:
+    while i > 0:
+        print('* '*i)
+        return draws(i-1)
+    
+def triangle(i, t=0):
+    if i == 0:
+        return 0
+    else:
+        print(' ' * ( t + 1 ) + '*' * ( i * 2 - 1 ))
+        return triangle( i - 1, t + 1 )
