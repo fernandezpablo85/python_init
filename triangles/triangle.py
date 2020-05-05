8bb9eeaba699329031034131b51d4df4eafc83a7
taniavill <tvillanueva@despegar.com>
2020-05-05 13:12:15 -0300

    Update triangle.py
    se actualiza función y pasa los test. para dibujar el triángulo se tiene que llamar a la función con print

--- triangles/triangle.py
+++ triangles/triangle.py
@@ -2,11 +2,10 @@ def draw(n):
     triangulo = []
     for i in range (0,n):
         triangulo.append(line(i+1))
-    print('\n'.join(triangulo))
-    #return '\n'.join(triangulo)
+    return '\n'.join(triangulo)
 
 def line(x):
-    linea = []
+    new_line = []
     for i in range(0,x):
-        linea.append('*')
-    return ' '.join(linea)    
+        new_line.append('*')
+    return ' '.join(new_line)    
