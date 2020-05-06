0f6cedc2df331d1d2af50dbb757a4d9e5a45b47f
taniavill <tvillanueva@despegar.com>
2020-05-06 18:25:24 -0300

    Create and test most_popular function

--- numbers/number.py
+++ numbers/number.py
@@ -1,20 +1,40 @@
 def add(a, b):
-    pass
+    return a + b
 
 
 def is_even(n):
-    pass
+    return n % 2 == 0
 
 
 def is_odd(n):
-    pass
+    return n % 2 != 0
 
 
-def sum_all(arr):
-    pass
+def sum_all(*arr):
+    if len(arr) == 0:
+        return 0
 
+    nums = arr[0] if isinstance(arr[0], list) else arr
+    return sum(nums)
 
 def most_popular(arr):
+    repetitions = {}
+    max_repetition = 0
+    popular = None
+    for numb in arr:
+        if numb not in repetitions:
+            repetitions[numb] = 1
+        else:
+            repetitions[numb] += 1
+        if repetitions[numb] > max_repetition:
+            max_repetition = repetitions[numb]
+            popular = numb
+        if repetitions[numb] == max_repetition:
+            popular = numb if numb < popular else popular
+    print(repetitions,max_repetition)
+    return popular
+
+    
     """Return the number that appears more times in the array (the most popular one). If there are
     two numbers that appear the same number of times, return the lowest
     
--- numbers/number_test.py
+++ numbers/number_test.py
@@ -3,12 +3,10 @@ import pytest
 import number
 
 
-@pytest.mark.skip()
 def test_sum_two_numbers():
     assert number.add(1, 2) == 3
 
 
-@pytest.mark.skip()
 def test_is_even():
     assert number.is_even(2)
     assert number.is_even(0)
@@ -16,7 +14,6 @@ def test_is_even():
     assert not number.is_even(123421)
 
 
-@pytest.mark.skip()
 def test_is_odd():
     assert not number.is_odd(0)
     assert number.is_odd(1)
@@ -32,21 +29,18 @@ def test_is_prime():
     assert not number.is_prime(9)
 
 
-@pytest.mark.skip()
 def test_sum_all_array():
     assert number.sum_all([1, 2, 3, 4]) == 10
     assert number.sum_all([1]) == 1
     assert number.sum_all([1, 1, -1, -1]) == 0
 
 
-@pytest.mark.skip()
 def test_sum_all_array_varags():
     assert number.sum_all(1, 2, 3, 4) == 10
     assert number.sum_all(1) == 1
     assert number.sum_all(1, 1, -1, -1) == 0
 
 
-@pytest.mark.skip()
 def test_most_popular():
     assert number.most_popular([1]) == 1
     assert number.most_popular([1, 2]) == 1
