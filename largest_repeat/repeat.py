135dfd3d676f765a29b2f0903a3a3581427891bd
taniavill <tvillanueva@despegar.com>
2020-05-13 10:28:33 -0300

    Update repeat.py
    largest_repeat in new branch

--- largest_repeat/repeat.py
+++ largest_repeat/repeat.py
@@ -1,10 +1,7 @@
 def largest_repeat(numbers):
-    """ Given an array of numbers, return the biggest value (x) that occurs exactly x times
+    largest = 0
+    for numb in numbers:
+        if numbers.count(numb) == numb:
+            largest = numb if numb > largest else largest
+    return largest
     
-    Examples: 
-    
-    * [1, 2, 3, 4] => 1 (the only one that occurs the same number of times)
-    * [1, 2, 2, 3, 3] => 2
-    * [1, 2, 3, 3, 3, 4, 4, 4, 4] => 4 (3 occurs 3 times and 4 occurs 4 times but 4 > 3)
-    """
-    pass
