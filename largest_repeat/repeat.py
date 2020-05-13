2373b1bf530b52c221a7118eb89c116097c543ab
taniavill <tvillanueva@despegar.com>
2020-05-13 14:28:17 -0300

    largest_repeat with counter

--- largest_repeat/repeat.py
+++ largest_repeat/repeat.py
@@ -1,7 +1,17 @@
-def largest_repeat(numbers):
+def largest_repeat_no_count(numbers):
     largest = 0
     for numb in numbers:
         if numbers.count(numb) == numb:
             largest = numb if numb > largest else largest
     return largest
     
+def largest_repeat(numbers):
+    largest = 0
+    for numb in numbers:
+        counter = 0
+        for n in numbers:
+            if n == numb:
+                counter += 1
+        if counter == numb:
+            largest = numb if numb > largest else largest
+    return largest
