9486cedf8216df895d3cff40a62dae25f033b10d
santiagokazlauskas12 <63759091+santiagokazlauskas12@users.noreply.github.com>
2020-05-24 17:36:02 -0300

    Seguro hay una manera mas elegante de resolverlo,pero paso los test

--- triangles/triangle.py
+++ triangles/triangle.py
@@ -1,11 +1,15 @@
-def draw(n):
-    """draws a triangle of n rows, for example for n == 5:
 
-    *
-    * *
-    * * *
-    * * * *
-    * * * * *
-    """
-    # implement this.
-    return "TODO"
+
+
+def draw(n):
+    w=[]
+    triangle_steps=[]
+    for stars in range(1,n+1):
+        step=("*"*stars)
+        for single_star in step:
+            w.append(single_star)
+        z=w
+        w=[]
+        t=" ".join(z)
+        triangle_steps.append(t)
+    return "\n".join(triangle_steps)
